6507bb1ebf4b43ec02c1e51cb435dfd91f600758
Wayne Graham <wgraham@clir.org>
2024-06-27 09:39:09 -0400

    Update for rendering image chunks

--- .eleventy.js
+++ .eleventy.js
@@ -27,7 +27,7 @@ function generateImages(src, widths) {
 	let options = {
 		widths: widths,
 		formats: ['webp', 'jpeg'],
-		outputDir: './_site/assets/images/',
+		outputDir: '../_site/assets/images/',
 		urlPath: '/assets/images/',
 		useCache: true,
 		sharpJpegOptions: {
